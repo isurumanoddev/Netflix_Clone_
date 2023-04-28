e87ff8c076a6ff91195c082afbf6eb9584460abc
isurumanod <isurumanod99@gmail.com>
2023-04-29 04:01:59 +0530

    urls

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function getOriginals() {
+    const url =
+        'https://p.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
+ 
+
+}
+
+function getTrendingNow() {
+    const url =
+        'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045';
+  
+}
+
+function getTopRated() {
+    const url =
+        'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1';
+   
+
+}
+
