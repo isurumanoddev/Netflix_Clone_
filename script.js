dcdf22db49178ab4ebcbd4913f703b639c3ec298
isurumanod <isurumanod99@gmail.com>
2023-04-29 04:19:37 +0530

    fetch Movies

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
+const getMovies = async (url) => {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+
+        // console.log()
+        showMovies(data["results"])
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+
 
 function getOriginals() {
     const url =
         'https://p.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
- 
 
+    getMovies(url)
 }
 
 function getTrendingNow() {
     const url =
         'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045';
-  
+    getMovies(url)
 }
 
 function getTopRated() {
     const url =
         'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1';
-   
 
+    getMovies(url)
 }
 
