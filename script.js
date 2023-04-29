e9e5019b96e5cc921bb4c9adf336d30d455a7e91
isurumanod <isurumanod99@gmail.com>
2023-04-29 07:27:16 +0530

    modal show

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+const getMovieTrailer = async (id) => {
+    const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`;
+
+    try {
+        const response = await fetch(url)
+        return await response.json()
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+const setTrailer = (trailers) => {
+    const iframe = document.getElementById("movieTrailer");
+    const movieNotFound = document.querySelector(".movieNotFound");
+
+    if (trailers.length > 0) {
+        movieNotFound.classList.add('d-none');
+        iframe.classList.remove('d-none');
+        iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`;
+
+    } else {
+        iframe.classList.add('d-none')
+        movieNotFound.classList.remove('d-none')
+    }
+};
+
+const handleMovieSelection = (e) => {
+    const id = e.target.getAttribute("data-id");
+    const iframe = document.getElementById('movieTrailer')
+
+    getMovieTrailer(id).then(data => {
+        console.log("data      :  ", data)
+        const results = data["results"]
+        const ytTrailer = results.filter(result => {
+            if (result.site === 'YouTube' && result.type === "Trailer") {
+                return true;
+            } else {
+                return false
+            }
+        })
+        setTrailer(ytTrailer)
+
+    })
+    var j = jQuery.noConflict();
+
+// Now you can use the 'j' variable instead of the '$' sign
+    j('#trailerModal').modal('show');
+ 
+  
+
+
+};
 const getMovies = async (url, dom_element) => {
     try {
         const response = await fetch(url)
@@ -41,6 +95,13 @@ const showMovies = (movies, dom_element) => {
         const posterImage = document.createElement("img");
         posterImage.setAttribute("data-id", movie["id"])
         posterImage.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
+
+
+        posterImage.addEventListener("click", e => {
+            handleMovieSelection(e)
+        });
+
+
         movieEl.appendChild(posterImage)
     }
 
@@ -57,31 +118,5 @@ window.onload = () => {
 
 //imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`
 
-const getMovieTrailer = async (id) => {
-    const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`;
 
-    try {
-        const response = await fetch(url)
-        const result = await response.json()
-        console.log(result)
 
-    }catch (error) {
-        console.log(error)
-    }
-
-}
-
-const setTrailer = (trailers) => {
-    const iframe = document.getElementById("movieTrailer");
-    const movieNotFound = document.querySelector(".movieNotFound");
-
-    if (trailers.length > 0) {
-        movieNotFound.classList.add('d-none');
-        iframe.classList.remove('d-none');
-        iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`;
-
-    } else {
-        iframe.classList.add('d-none')
-    movieNotFound.classList.remove('d-none')
-    }
-};
