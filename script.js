62833d5359949cc9de1c10ed8a1fd1c5c03101b5
isurumanod <isurumanod99@gmail.com>
2023-04-29 06:37:20 +0530

    get movies and set trailer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const getMovies = async (url, dom_element) => {
     try {
         const response = await fetch(url)
         const data = await response.json()
-        
+
         // console.log()
         showMovies(data["results"], dom_element)
     } catch (error) {
@@ -22,14 +22,14 @@ function getOriginals() {
 function getTrendingNow() {
     const url =
         'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045';
-    getMovies(url, ".original__movies")
+    getMovies(url, "#trending")
 }
 
 function getTopRated() {
     const url =
         'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1';
 
-    getMovies(url, ".original__movies")
+    getMovies(url, "#top_rated")
 }
 
 const showMovies = (movies, dom_element) => {
@@ -50,9 +50,38 @@ const showMovies = (movies, dom_element) => {
 window.onload = () => {
     getTopRated()
     getTrendingNow()
-    getOriginals()
+    // getOriginals()
 
 
 }
 
 //imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`
+
+const getMovieTrailer = async (id) => {
+    const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US`;
+
+    try {
+        const response = await fetch(url)
+        const result = await response.json()
+        console.log(result)
+
+    }catch (error) {
+        console.log(error)
+    }
+
+}
+
+const setTrailer = (trailers) => {
+    const iframe = document.getElementById("movieTrailer");
+    const movieNotFound = document.querySelector(".movieNotFound");
+
+    if (trailers.length > 0) {
+        movieNotFound.classList.add('d-none');
+        iframe.classList.remove('d-none');
+        iframe.src = `https://www.youtube.com/embed/${trailers[0].key}`;
+
+    } else {
+        iframe.classList.add('d-none')
+    movieNotFound.classList.remove('d-none')
+    }
+};
