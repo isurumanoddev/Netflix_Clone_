5579f647d35e4a6c3530043a1b0628fb5aeee228
isurumanod <isurumanod99@gmail.com>
2023-04-29 05:49:30 +0530

    show movie poster on dom

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const getMovies = async (url) => {
+const getMovies = async (url, dom_element) => {
     try {
         const response = await fetch(url)
         const data = await response.json()
-
+        
         // console.log()
-        showMovies(data["results"])
+        showMovies(data["results"], dom_element)
     } catch (error) {
         console.log(error)
     }
@@ -16,19 +16,43 @@ function getOriginals() {
     const url =
         'https://p.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213'
 
-    getMovies(url)
+    getMovies(url, ".original__movies")
 }
 
 function getTrendingNow() {
     const url =
         'https://api.themoviedb.org/3/trending/movie/week?api_key=19f84e11932abbc79e6d83f82d6d1045';
-    getMovies(url)
+    getMovies(url, ".original__movies")
 }
 
 function getTopRated() {
     const url =
         'https://api.themoviedb.org/3/movie/top_rated?api_key=19f84e11932abbc79e6d83f82d6d1045&language=en-US&page=1';
 
-    getMovies(url)
+    getMovies(url, ".original__movies")
 }
 
+const showMovies = (movies, dom_element) => {
+
+    const movieEl = document.querySelector(dom_element);
+
+    for (const movie of movies) {
+        // console.log(movie["poster_path"])
+        const posterImage = document.createElement("img");
+        posterImage.setAttribute("data-id", movie["id"])
+        posterImage.src = `https://image.tmdb.org/t/p/original${movie["poster_path"]}`
+        movieEl.appendChild(posterImage)
+    }
+
+
+}
+
+window.onload = () => {
+    getTopRated()
+    getTrendingNow()
+    getOriginals()
+
+
+}
+
+//imageElement.src = `https://image.tmdb.org/t/p/original${movie[path_type]}`
